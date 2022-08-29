2a57a79efc2fa5f870232cb6e40534492a9917fb
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-08-29 14:10:03 +0200

    Censurato il testo con la parola: Lorem. Esercizio completato

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+// Paragrafo 
 $paragraph = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-Architecto nam possimus quam maxime vero neque eum at quo iure. Non deleniti 
-possimus accusantium, sequi minus autem vitae unde perspiciatis itaque.";
+Architecto nam possimus quam maxime vero neque eum at quo iure.";
 
+// Lunghezza del paragrafo
 $paragraph_length = strlen($paragraph);
+
+// Prelevo la query string
+$bad_word = $_GET['badword'];
+
+// Faccio l'up della prima lettera della parola perchè avevo passato Lorem
+// $up_bad_word = ucfirst($bad_word);
+
+// Censuro il poaragrafp
+$paragraph_censured = str_replace($bad_word,'*****',$paragraph);
 ?>
 
 <!DOCTYPE html>
@@ -19,5 +29,9 @@ $paragraph_length = strlen($paragraph);
     <p><?= $paragraph ?></p>
     <!-- Lunghezza del paragrfo calcolata con un metodo PHP -->
     <div>Lunghezza del paragrafo: <?= $paragraph_length ?></div>
+    <!-- Parola da censurare -->
+    <div>Parola da censurare: <?= $bad_word ?></div>
+    <!-- Testo censurato -->
+    <div>Testo censurato: <?= $paragraph_censured ?></div>
 </body>
 </html>
